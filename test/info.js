57ffa66a170908123ac29587729a4c66665bce83
Chuck LeDuc Díaz <chuck@caelumlabs.com>
2020-04-21 19:07:30 +0200

    add gitInfoForNetwork()

--- test/info.js
+++ test/info.js
@@ -3,12 +3,19 @@ const chai = require('chai')
 const expect = chai.expect
 
 describe('blockchain info (type, endpoints) for did', () => {
-  it('should get the info on the "and" network', () => {
+  it('should get the info for the "labdev" network', () => {
+    const info = LorenaDidResolver.getInfoForNetwork('labdev')
+    expect(info).to.not.be.undefined
+    expect(info.type).to.eq('substrate')
+    expect(info.blockchainEndpoint).to.contain('labdev')
+  })
+
+  it('should get the info for a did on the "and" network', () => {
     const info = LorenaDidResolver.getInfoForDid('did:lor:and:UVRaa2VFOURZVjk0YkVsV1pGZHhhR3RU')
     expect(info).to.not.be.undefined
   })
 
-  it('should get undefined for the "xxx" network', () => {
+  it('should get undefined for a did on the "xxx" network', () => {
     const info = LorenaDidResolver.getInfoForDid('did:lor:xxx:UVRaa2VFOURZVjk0YkVsV1pGZHhhR3RU')
     expect(info).be.undefined
   })
